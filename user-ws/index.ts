39ec2493a40ecb672233fa8ea8620a642c0eac99
Diwakar Singh <aryan90067@gmail.com>
2025-07-21 00:52:48 +0530

    Added user web socket, relayer web socket

---
+++ user-ws/index.ts
@@ -0,0 +1,34 @@
+import { WebSocketServer, WebSocket } from 'ws';
+
+const wss = new WebSocketServer({ port: 8080 });
+
+interface Room {
+   sockets: WebSocket[],
+}
+const rooms: Record<string, Room> = {}
+
+wss.on('connection', function connection(ws) {
+   ws.on('error', console.error);
+
+   ws.on('message', function message(data: string) {
+      console.log('received: %s', data);
+      const parsedData = JSON.parse(data)
+      if (parsedData.type == "join-room") {
+         const room = parsedData.room
+         if (!rooms[room]) {
+            rooms[room] = {
+               sockets: []
+            }
+         }
+         rooms[room].sockets.push(ws)
+      }
+
+      if (parsedData.type == "chat") {
+         const room = parsedData.room;
+         rooms[room].sockets.map(socket => socket.send(data))
+
+      }
+   });
+
+   ws.send(JSON.stringify(rooms));
+}); 
